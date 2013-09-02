6eb69bbc9da545cbb6c202d191d6e196c2143b78
Amos Stone <amos@amosarts.com>
2013-09-02 14:47:12 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NcesLookup::Application.config.secret_key_base = '6d216d0f18949bb462235fc337739808100cbfd40e8de39c6c04e6f313d54239e0f454bd030e4a3f993af28d779ab732a89ba8ceb1299e0bc7a1db8aec8c065c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NcesLookup::Application.config.session_store :cookie_store, key: '_nces-lookup_session'
