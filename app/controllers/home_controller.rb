a279febdcf0a717d4b9528562f3770cb32c62b7a
Amos Stone <amos@amosarts.com>
2013-09-19 11:15:30 -0400

    Adds logger statements for debug ec2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
 
 	def index
+		logger.info @request_city
+		logger.info "---"
+		logger.info request
 		@schools = School.near(@request_city).limit(10)
 	end
 
